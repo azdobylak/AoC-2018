232e0a1bbff5cf26a13b69aecea0234a25dc6e05
Adrian Zdobylak <zdobylak.adrian@gmail.com>
2019-02-16 15:23:38 +0100

    day 05, part 02 - returns correct result, but is quite slow

--- src/main/scala/day05/Day05.scala
+++ src/main/scala/day05/Day05.scala
@@ -11,25 +11,47 @@ object Day05 {
 
     def main(args: Array[String]): Unit = {
         val line = lines.head
-        println(solve_first(line))
-        //println(solve_second(lines))
+        //println(solve_first(line))
+        println(solve_second(line))
     }
 
-    def solve_first(line: String): String = {
+    def solve_first(line: String): Int = {
+        reduce(line).length
+    }
+
+    def solve_second(line: String): Int = {
+        var lengthWithoutChar: Map[Char, Int] = Map.empty[Char, Int]
+        val chars = line.toLowerCase.toSet
+        for (c <- chars)
+            lengthWithoutChar += (c -> reduce(line, c).length)
+
+        lengthWithoutChar.minBy(_._2)._2
+    }
+
+    private def removeCharacters(string: String, char: Char): String = {
+        // remove all occurrences of given character, both upper and lowercase
+        val excludeChars = Set(char.toLower, char.toUpper)
+        string.filterNot(excludeChars)
+    }
+
+    def reduce(text: String, removeChar: Char): String = {
+        val trimmedText = removeCharacters(text, removeChar)
+        reduce(trimmedText)
+    }
+
+    def reduce(text: String): String = {
         def reduceString(input: Seq[Char], acc: Seq[Char]): String = {
-          input match {
-              case a+:b+:rest =>
-                  if (a != b && a.toLower == b.toLower)
-                      reduceString(acc ++ rest, Vector.empty[Char])
-                  else
-                      reduceString(b+:rest, acc :+ a)
-              case b+:IndexedSeq() => (acc :+ b).mkString
-              case IndexedSeq() => if (acc.isEmpty) "" else acc.toString
-          }
+            input match {
+                case a +: b +: rest =>
+                    if (a != b && a.toLower == b.toLower)
+                        reduceString(acc ++ rest, Vector.empty[Char])
+                    else
+                        reduceString(b +: rest, acc :+ a)
+                case b +: IndexedSeq() => (acc :+ b).mkString
+                case IndexedSeq() => if (acc.isEmpty) "" else acc.toString
+            }
         }
-        reduceString(line.toVector, Vector.empty[Char])
+        reduceString(text.toVector, Vector.empty[Char])
     }
 
-    //def solve_second(lines: String): Int = {
-    //}
 }
